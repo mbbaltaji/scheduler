0c79838bb9b9db74b10e35f1fa8ca6e4d6326564
mbbaltaji <baltaji@uwindsor.ca>
2022-05-19 13:42:31 -0400

    import className library and use it for conditional checking of component styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,25 @@
 import React from "react";
+import classNames from 'classnames';
 
 import "components/Button.scss";
 
 export default function Button(props) {
    console.log("props", props);
-   let buttonClass = "button";
 
-   if(props.confirm) {
-      buttonClass += " button--confirm";
-   }
+   const buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
 
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   
+
+   // if(props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
    return (
    <button 
       onClick={props.onClick} 
