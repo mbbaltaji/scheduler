838e63d88a395123756701d5bd55d8c9bc63d6ba
mbbaltaji <baltaji@uwindsor.ca>
2022-05-21 15:16:40 -0400

    refactor

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -9,8 +9,7 @@ export default function DayListItem(props) {
     'day-list__item--full' : props.spots === 0  // styles unavailable day list item
   });
 
-  function formatSpots() {  
-
+  function formatSpots() {
     if (props.spots === 0) {
       return <h3 className="text--light">no spots remaining</h3>;
     } else if (props.spots === 1){
@@ -18,9 +17,6 @@ export default function DayListItem(props) {
     } else {
       return <h3 className="text--light">{props.spots} spots remaining</h3> 
     }
-    // props.spots === 0 && <h3 className="text--light">no spots remaining</h3>
-    // props.spots === 1 && <h3 className="text--light">1 spot remaining</h3>
-    // props.spots > 1 && <h3 className="text--light">{props.spots} spot remaining</h3>
   }
   
   return (
