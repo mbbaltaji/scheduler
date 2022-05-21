2b322dae7f2ba1effe84a3ff00652359d711a9b3
mbbaltaji <baltaji@uwindsor.ca>
2022-05-21 15:15:53 -0400

    implement DayList component that will return a list of DayListItem compoenents as children

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import DayListItem from './DayListItem';
 
-function DayListItem() {
+
+//<DayList> component that will serve as a container for all the <DayListItem> components
+export default function DayList(props) {
+  // extracting each day object and mapping each day to DayListItem component
+  const days = props.days; 
+  const dayListItems = days.map((day) => {  
+    // return DayListITem as children of DayList
+    return (
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day} 
+      setDay={props.setDay}
+    />
+    );
+  })  
   return (
-    <ul></ul>
+    <ul>
+      {dayListItems}
+    </ul>
   );
 }
