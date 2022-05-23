b2dbf0cd8bee245bb34db3bfccafeb4554ecfb28
mbbaltaji <baltaji@uwindsor.ca>
2022-05-23 19:50:05 -0400

    add story for Header Component and pass props

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from 'components/InterviewerListItem'
 import InterviewerList from 'components/InterviewerList'
-import Appointment from 'components/Appointment/index'
+import Appointment from 'components/Appointment/index.js'
 
 
 storiesOf("Button", module)
@@ -144,4 +144,5 @@ storiesOf("InterviewerListItem", module)
       backgrounds: [{ name: "white", value: "#fff", default: true }]
     })
     .add("Appointment", () => <Appointment />)
-    .add('Appointment with Time', () => <Appointment time='12pm' />);
+    .add('Appointment with Time', () => <Appointment time='12pm' />)
+    .add("Header", () => <Header time="12pm" />)
