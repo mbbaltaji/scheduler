e1e9a4c8e896750cb77c0aca49629ac3ef80ff2d
mbbaltaji <baltaji@uwindsor.ca>
2022-05-23 20:29:10 -0400

    add story to test Confirm component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment/index.js'
 import Header from 'components/Appointment/Header'
 import Empty from 'components/Appointment/Empty'
 import Show from 'components/Appointment/Show'
+import Confirm from 'components/Appointment/Confirm'
 
 
 storiesOf("Button", module)
@@ -157,4 +158,11 @@ storiesOf("InterviewerListItem", module)
         onEdit={action("onEdit")}
         onDelete={action("onDelete")}
       />
+    )
+    .add("Confirm", () => 
+      < Confirm
+        message='Delete this appointment?'
+        onConfirm={action('onConfirm')}
+        onCancel={action('onCancel')}
+      />  
     )
