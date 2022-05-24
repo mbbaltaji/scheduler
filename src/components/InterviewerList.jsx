8c57a466a9caeb369eaa176bd3e9e4c7216d08aa
mbbaltaji <baltaji@uwindsor.ca>
2022-05-24 17:09:33 -0400

    refactor: destructure props object

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -4,9 +4,8 @@ import InterviewerListItem from './InterviewerListItem';
 
 
 export default function InterviewerList(props) {
-
-  const {onChange, value} = props;
-  const interviewers = props.interviewers;
+  const {onChange, value, interviewers} = props;
+  
   const interviewerListItems = interviewers.map((interviewer) => {
     return(
     <InterviewerListItem
@@ -18,6 +17,7 @@ export default function InterviewerList(props) {
     />
     );
   })
+  
   return(
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
