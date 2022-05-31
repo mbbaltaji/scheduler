dc0eac20b8493a9559fba083feed32f4f483c246
mbbaltaji <baltaji@uwindsor.ca>
2022-05-31 17:08:27 -0400

    add selector function to get appoinemts by day and jest testing for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day){
+  
+  if (!day) {
+    return [];
+  }
+
+  const days = state.days;
+  const foundDay = days.find( dayObj => dayObj.name === day);
+  
+  if (!foundDay) {
+    return [];
+  }
+  const foundAppointments = foundDay.appointments.map( id => {
+    return state.appointments[id];
+  });
+
+  return foundAppointments;
+}
