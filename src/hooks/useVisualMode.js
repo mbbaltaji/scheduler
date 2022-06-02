d685f8fde474a6df6165de758c7f014ed624b70a
mbbaltaji <baltaji@uwindsor.ca>
2022-06-02 18:41:14 -0400

    add custom hook to track each mode user is in

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial){
+  
+  // keeps track of each mode (EMPTY, SHOW, CREATE/EDIT, SAVING/DELETING, CONFIRM)
+  const [mode, setMode] = useState(initial);
+  // keeps track of each mode when user wants to go back to previous view
+  // e.g. after cancelling, or hitting the X icon
+  const [history, setHistory] = useState([initial]);
+
+  // allows use to advance to any other mode
+  function transition(newMode, replace = false) {
+
+  if (replace) {
+    setHistory(prev => {
+      const newHistory = [...prev];
+      newHistory[newHistory.length-1] = newMode 
+      return newHistory;
+    })
+  } else {
+    setHistory(prev => [...prev, newMode]);
+  }
+  setMode(newMode);
+}
+
+  // allows user to return to previous mode (e.g canceling)
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => { 
+       const newHistory = [...prev];
+       newHistory.pop();
+       return newHistory;
+      })
+    }
+
+  }
+  return { mode, transition, back};
+
+ 
+}
+
+
