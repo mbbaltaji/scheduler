749ed193ba5545c818ab8617f67b74b00fd5b341
mbbaltaji <baltaji@uwindsor.ca>
2022-06-02 18:42:55 -0400

    render add appointment view

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,36 @@ import  'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header';
 import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import useVisualMode from '../../hooks/useVisualMode';
 
-
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 export default function Appointment (props) {
-  const {time, interview} = props;
+
+  const { mode, transition, back} = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  )
+  const {time, interview, student, interviewers} = props;
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === SHOW && (
+      <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+      />
+)}
+
+      {mode === CREATE && 
+      <Form 
+        interviewers={[]}
+        onSave={() =>  console.log('hi')}
+        onCancel={() => back()}
+      /> }
+
     </article>
   )
 }
