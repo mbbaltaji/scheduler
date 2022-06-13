8b0f79fc84140d4597759c85b923170ee26c1c0b
mbbaltaji <baltaji@uwindsor.ca>
2022-06-13 16:18:43 -0400

    add prop-type testing library as a dependency

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -9,7 +9,7 @@ export default function DayListItem(props) {
     'day-list__item--full' : props.spots === 0  // styles unavailable day list item
   });
 
-  function formatSpots() {
+  function formatSpots(spots) {
     if (props.spots === 0) {
       return <h3 className="text--light">no spots remaining</h3>;
     } else if (props.spots === 1){
@@ -22,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      {formatSpots()}
+      {formatSpots(props.spots)}
     </li>
   );
 }
--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from './InterviewerListItem';
-
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const {onChange, value, interviewers} = props;
@@ -28,4 +28,8 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
